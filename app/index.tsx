164a38e2166aca0284ee728deb4659f95eed8e61
Moazam05 <salmanmoazam08@gmail.com>
2024-08-23 20:13:05 +0500

    Clean Code delete startup files

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,12 @@ import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const HomeScreen = () => {
+const Home = () => {
   return (
     <SafeAreaView>
-      <Text className="text-3xl text-red-500">HomeScreen</Text>
+      <Text>Home</Text>
     </SafeAreaView>
   );
 };
 
-export default HomeScreen;
+export default Home;
