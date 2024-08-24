9b4f707456b77852093a3ddd75707b3fe486b500
Moazam05 <salmanmoazam08@gmail.com>
2024-08-24 20:06:13 +0500

    Image show & text

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import React, { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
@@ -30,8 +30,23 @@ const Welcome = () => {
       >
         {onboarding.map((item) => {
           return (
-            <View key={item.id}>
-              <Text>{item.title}</Text>
+            <View
+              key={item.id}
+              className="flex items-center justify-center p-5"
+            >
+              <Image
+                source={item.image}
+                resizeMode="contain"
+                className="w-full h-[300px]"
+              />
+              <View className="flex flex-row items-center justify-center w-full mt-10">
+                <Text className="text-black text-3xl font-bold mx-10 text-center">
+                  {item.title}
+                </Text>
+              </View>
+              <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+                {item.description}
+              </Text>
             </View>
           );
         })}
