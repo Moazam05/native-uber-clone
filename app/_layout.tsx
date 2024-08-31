334d0ae103464d5863f951d504dfb6ca13152803
Moazam05 <salmanmoazam08@gmail.com>
2024-08-31 14:08:59 +0500

    fonts issue fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,55 +1,50 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect, useState } from "react";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "react-native-reanimated";
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { LogBox } from "react-native";
+
 import { tokenCache } from "@/lib/auth";
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    // eslint-disable-next-line prettier/prettier
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
+
+LogBox.ignoreLogs(["Clerk:"]);
+
 export default function RootLayout() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const [fontsLoaded, error] = useFonts({
+  const [loaded] = useFonts({
     "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
     "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
     "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
     "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
     "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
   useEffect(() => {
-    async function prepare() {
-      try {
-        if (error) throw error;
-        if (fontsLoaded) {
-          await SplashScreen.hideAsync();
-          setAppIsReady(true);
-        }
-      } catch (e) {
-        console.warn(e);
-      }
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    prepare();
-  }, [fontsLoaded, error]);
-
-  if (!appIsReady) {
-    return null; // Render nothing until the fonts are loaded and the app is ready
-  }
-
-  const publishableKey =
-    process.env.NODE_ENV === "production"
-      ? process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-      : process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-  if (!publishableKey) {
-    throw new Error(
-      `Missing Publishable Key. Please set the correct Clerk Publishable Key in your environment variables.`
-    );
+  if (!loaded) {
+    return null;
   }
 
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <ClerkLoaded>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
