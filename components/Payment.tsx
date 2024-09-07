670d2e8028592f71b41065d84615704379eacc7f
Moazam05 <salmanmoazam08@gmail.com>
2024-09-07 15:31:04 +0500

    stripe added

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomButton from "./CustomButton";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { Alert } from "react-native";
 
 const Payment = () => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+
+  const [success, setSuccess] = useState(false);
+
+   const confirmHandler = async (paymentMethod, shouldSavePaymentMethod, intentCreationCallback) => {
+    // Make a request to your own server.
+    const myServerResponse = await fetch(...);
+    // Call the `intentCreationCallback` with your server response's client secret or error
+    const { clientSecret, error } = await response.json();
+    if (clientSecret) {
+      intentCreationCallback({clientSecret})
+    } else {
+      intentCreationCallback({error})
+    }
+  }
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+
+
   const openPaymentSheet = async () => {
-    console.log("Opening payment sheet");
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      if (error.code === PaymentSheetError.Canceled) {
+        Alert.alert(`Error code: ${error.code}`, `${error.message}`);
+      }
+    } else {
+      setSuccess(true);
+    }
   };
 
   return (
