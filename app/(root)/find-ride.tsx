09393101fddf5f4e5dc4c22c004a1b99482bf3a6
Moazam05 <salmanmoazam08@gmail.com>
2024-09-07 12:31:58 +0500

    new routes define

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,11 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useUserLocationStore } from "@/store";
+import { SafeAreaView } from "react-native-safe-area-context";
+import RideLayout from "@/components/RideLayout";
 
 const FindRide = () => {
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useUserLocationStore();
+
   return (
-    <View>
-      <Text>FindRide</Text>
-    </View>
+    <RideLayout>
+      <Text className="text-2xl">Find Ride</Text>
+    </RideLayout>
   );
 };
 
