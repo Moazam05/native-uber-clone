447c759782a0cec870f5bdff0dc2693b3899dc5b
Moazam05 <salmanmoazam08@gmail.com>
2024-09-08 19:38:28 +0500

    sentry logger added

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,22 @@ import React from "react";
 import { Redirect } from "expo-router";
 // Clerk for Authentication
 import { useAuth } from "@clerk/clerk-expo";
+import * as Sentry from "@sentry/react-native";
 
 const Home = () => {
   const { isSignedIn } = useAuth();
+
+  Sentry.init({
+    dsn: "https://8ded61bfb686a3a17377e497101e88be@o4507917711900672.ingest.de.sentry.io/4507917721010256",
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+    _experiments: {
+      // profilesSampleRate is relative to tracesSampleRate.
+      // Here, we'll capture profiles for 100% of transactions.
+      profilesSampleRate: 1.0,
+    },
+  });
 
   if (isSignedIn) {
     return <Redirect href={"/(root)/(tabs)/home"} />;
@@ -15,4 +28,4 @@ const Home = () => {
   return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Home;
+export default Sentry.wrap(Home);
